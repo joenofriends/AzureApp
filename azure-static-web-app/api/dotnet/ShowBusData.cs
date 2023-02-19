32370f4aad3326086baa4354011563f79d06980d
Josh <someguyperson@hotmail.com>
2023-02-19 14:54:39 +1000

    take 8

--- azure-static-web-app/api/dotnet/ShowBusData.cs
+++ azure-static-web-app/api/dotnet/ShowBusData.cs
@@ -101,9 +101,9 @@ namespace ShowBusData
             int rid = 4, gid = 0, bean = 0;
 
 
-            int drillId = null;
+            int drillId = 0;
             string date= null;
-            int dayOrNight= null;
+            int dayOrNight= 0;
             /*
             var DewateringDelay= 0.0;
             var FishingDelay=0.0;
